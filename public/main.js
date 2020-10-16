f9c020596795598efcefcf5c3b018b792e7ea03d
nicolasae <nicolas.aguirre@utp.edu.co>
2020-10-16 15:49:06 -0500

    se guardan los datos

--- public/main.js
+++ public/main.js
@@ -1,15 +1,18 @@
 /*conectamos el cliente con el servidor de websocket*/
 var socket = io.connect('http://localhost:8080',{'forceNew':true});
+var datos = [];
 
 /*recibiendo mensajes*/
 socket.on('messages',function(data){
     console.log(data);
     render(data);
+    datos.push(data.state);
+    console.log(datos);
+    //console.log(typeof data.state)
 });
 
 function render (data) {
     var html = data.map(function(elem, index) {
-        //<em>${elem.res}</em>
         return(`<div>
                   <strong>${elem.state}</strong>:
                  
