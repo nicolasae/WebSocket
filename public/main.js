7a5b9cd9cb1cf176497652f75b566496d7d62d9b
Jorge Orobio Auz <j.orobio@utp.edu.co>
2020-10-16 16:24:26 -0500

    wip

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 /*conectamos el cliente con el servidor de websocket*/
 var socket = io.connect('http://localhost:8080',{'forceNew':true});
+//var Chart = require('chart.js')
 var datos = [];
 
 /*recibiendo mensajes*/
@@ -11,32 +12,48 @@ socket.on('messages',function(data){
     //console.log(typeof data.state)
 });
 
+socket.on('graphic',function(data){
+    graficador(data);
+});
+
+function graficador(data){
+    var html = data.map(function(elem, index) {
+        return(`<div>
+                <canvas id="myChart" width="400" height="400"></canvas>                 
+                <script>
+                ${elem}
+                </script> 
+                </div>`);
+    }).join(" ");
+    document.getElementById('grapchic').innerHTML = html;
+}
+
 function render (data) {
     var html = data.map(function(elem, index) {
         return(`<div>
                   <strong>${elem.state}</strong>:
-                 
                 </div>`);
     }).join(" ");
     document.getElementById('messages').innerHTML = html;
 }
 
-function addMessage(respuesta) {
+function addMessage(respuesta,boton) {
+    if (boton == true ){
+        var message = {
+            state: respuesta 
+        };  
+    }else {
       var message = {
         state: document.getElementById('numero').value,
         res: respuesta 
-    };
+    }};
     
     socket.emit('new-message', message);
     return false;
 }
 
-function makegraphic(respuesta) {
-    var message = {
-      state: 500,
-      res: respuesta 
-  };
-  
-  socket.emit('new-graphic', message);
+function makegraphic() {
+     
+  socket.emit('new-graphic', datos);
   return false;
 }
