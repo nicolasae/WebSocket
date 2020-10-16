7605f764541640a7c4b7353762e39d1155c5399b
Jorge Orobio Auz <j.orobio@utp.edu.co>
2020-10-16 17:39:42 -0500

    wip

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
+var Chart = require('chart.js');
 /*conectamos el cliente con el servidor de websocket*/
 var socket = io.connect('http://localhost:8080',{'forceNew':true});
-//var Chart = require('chart.js')
+
 var datos = [];
 
 /*recibiendo mensajes*/
@@ -17,15 +18,42 @@ socket.on('graphic',function(data){
 });
 
 function graficador(data){
-    var html = data.map(function(elem, index) {
-        return(`<div>
-                <canvas id="myChart" width="400" height="400"></canvas>                 
-                <script>
-                ${elem}
-                </script> 
-                </div>`);
-    }).join(" ");
-    document.getElementById('grapchic').innerHTML = html;
+    var myChart = new Chart(document.getElementById('myChart'), {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: data.data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
 
 function render (data) {
@@ -53,7 +81,9 @@ function addMessage(respuesta,boton) {
 }
 
 function makegraphic() {
-     
-  socket.emit('new-graphic', datos);
+var message = {
+    data : datos
+}
+  socket.emit('new-graphic', message);
   return false;
 }
--- server/main.js
+++ server/main.js
@@ -27,9 +27,8 @@ io.on('connection',function(socket){
         messages.push(data);    
     io.sockets.emit('messages', messages);
     });
-    socket.on('new-graphic', function(data) {
-        chart = graficar(data);    
-    io.sockets.emit('graphic', chart )
+    socket.on('new-graphic', function(data) {    
+    io.sockets.emit('graphic', data )
     });
 });
 function random(res){
@@ -46,47 +45,7 @@ function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low)
 };
 
-function graficar(lista){
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: lista,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-return myChart;
-};
 /*Se verifica el funcionamiento del servidor, puerto 8080*/
 server.listen(8080,function(){
     console.log('Servidor corriendo en http://localhost:8080')
