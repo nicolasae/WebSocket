261134b9badbf5c13e4b3085373ed25b6d1fcfe8
Jorge Orobio Auz <j.orobio@utp.edu.co>
2020-10-15 22:46:03 -0500

    wip

--- public/main.js
+++ public/main.js
@@ -4,4 +4,26 @@ var socket = io.connect('http://localhost:8080',{'forceNew':true});
 /*recibiendo mensajes*/
 socket.on('messages',function(data){
     console.log(data);
+    render(data);
 });
+
+function render (data) {
+    var html = data.map(function(elem, index) {
+        return(`<div>
+                  <strong>${elem.id}</strong>:
+                  <em>${elem.text}</em>
+                </div>`);
+    }).join(" ");
+    document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {
+      var message = {
+        id: document.getElementById('id').value,
+        text: document.getElementById('texto').value
+    };
+    
+    socket.emit('new-message', message);
+    return false;
+}
+
--- server/main.js
+++ server/main.js
@@ -12,17 +12,34 @@ app.use(express.static('public'))
 app.get('/',function(req,res){
     res.status(200).send('Hola mundo');
 });
+var messages = [{
+    id: 1,
+    text: random(),
+    res: " "
+    }];
 
 /*con el comando on se le dice que escuche algo del navegador o del serivor*/
 io.on('connection',function(socket){
     console.log('Alguien se ha conectado con Sockets');
-    socket.emit('messages',{
-        id:1,
-        text:"Hola soy un mensaje",
-
-    })
+    socket.emit('messages',messages )
+    socket.on('new-message', function(data) {
+        messages.push(data);    
+    io.sockets.emit('messages', messages);
+    });
 });
+function random(res){
+if (res == "bajo" ){
+    return (randomInt( 500, 1000))
+};
+if ( res == "alto" ){
+    return (randomInt( 1, 500))
+};
+return (randomInt( 1, 1000));
+};
 
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low)
+};
 
 /*Se verifica el funcionamiento del servidor, puerto 8080*/
 server.listen(8080,function(){
